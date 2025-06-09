0c74c87203272ef49af532f20c940f242644ecf5
Chenx Dayat <dayatbwi@gmail.com>
2025-06-09 09:43:27 +0700

    Create dplayer.js

---
+++ dplayer.js
@@ -0,0 +1,11 @@
+const channels = {
+  dazn1de: {
+    url: "https://nijineh-lalajo.hf.space/watch/aHR0cHM6Ly9kZHk2bmV3Lm5ld2tzby5ydS9kZHk2L3ByZW1pdW00MjYvbW9uby5tM3U4.m3u8",
+    type: "hls"
+  },
+  dazn2de: {
+    url: "https://nijineh-lalajo.hf.space/watch/aHR0cHM6Ly9kZHk2bmV3Lm5ld2tzby5ydS9kZHk2L3ByZW1pdW00MjcvbW9uby5tM3U4.m3u8",
+    type: "hls"
+  },
+  // Tambah channel lain di sini
+};
